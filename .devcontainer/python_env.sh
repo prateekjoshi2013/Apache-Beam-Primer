8df9b9a5f49a659e4c20560fc9b5440f51ba945d
prateek <prateekjoshi2013@gmail.com>
2024-06-29 00:37:42 +0000

    inital commit

---
+++ .devcontainer/python_env.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+python3 -m venv /python_env
+source /python_env/bin/activate
+pip install -r /scripts/requirements.txt
---
+++ README.md
@@ -0,0 +1,10 @@
+# create venv
+
+```sh
+python3 -m venv myenv
+```
+# Activate myenv env
+
+```sh
+source myenv/bin/activate
+```
